639c60cc71305a7700eaa39373f122fca99869ac
Collins Tatang <makungongcollins@gmail.com>
2023-03-22 15:52:08 +0100

    Add Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import PostPage from "./Pages/PostPage";
 
 function App() {
+
+  const[getBlogContent, setGetBlogContent] = useState([])
+  const getData = (blog) => {
+    setGetBlogContent(blog);
+  }
   return (
-    <div>
-     App
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<HomePage data={getData}/>} />
+        <Route path="/blog/:id" element={<PostPage data={getData}/>} />
+      </Routes>
     </div>
   );
 }
