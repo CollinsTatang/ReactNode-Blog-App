4d4fa2f7c16e846d51bfa4dd971e00ad08d6279d
Collins Tatang <makungongcollins@gmail.com>
2023-09-18 17:11:50 +0100

    Fix routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Post from "./Pages/Post";
+import About from "./Pages/About";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -6,6 +12,13 @@ function App() {
   return (
     <div className="container">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
         <Footer />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,30 @@
 import React from "react";
-import { Link, Route, Routes } from 'react-router-dom';
-import Home from "../Pages/Home";
-import Login from "../Pages/Login";
-import Register from "../Pages/Register";
-import Post from "../Pages/Post";
-import About from "../Pages/About";
+import { Link} from 'react-router-dom';
+import '../css/Navbar.css'
 
 const Navbar = () => {
     return(
-        <header className='home-header'>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/post">Post</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-       </header>
+        <header>
+            <nav className="navbar">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/post">Post</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Register</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 export default Navbar;
