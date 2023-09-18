1038148ef5431ea3fd933fc2d40002c47bce65c8
Collins Tatang <makungongcollins@gmail.com>
2023-09-18 15:57:30 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="container">
         <Navbar />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return(
+        <div>
+            <h3>Footer Pages</h3>
+        </div>
+    )
+}
+export default Footer;
